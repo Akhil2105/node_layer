4091d00a89ce09e6a2b93d128f79e3dfcf2996f6
Akhil2105 <86476708+Akhil2105@users.noreply.github.com>
2024-01-21 16:07:05 -0500

    Update server.js
    added new service

--- server.js
+++ server.js
@@ -38,6 +38,18 @@ app.post('/register', (req, res) => {
     }
   });
 
+app.post('/reactivelogin', (req, res) => {
+  const { username, password } = req.body;
+  // Simulate user login
+  const user = users.find(u => u.username === username && u.password === password);
+  if (user) {
+    res.json({ message: 'Login successful', user });
+  } else {
+    res.status(401).json({ message: 'Invalid credentials' });
+  }
+});
+
+
 // Get user by ID endpoint
 app.get('/user/:id', (req, res) => {
     const userId = parseInt(req.params.id);
@@ -52,7 +64,7 @@ app.get('/user/:id', (req, res) => {
 
 app.get('/userList', (req, res) => {
     
-    ;
+    
     res.status(200).json({ result: users });
 
   });
